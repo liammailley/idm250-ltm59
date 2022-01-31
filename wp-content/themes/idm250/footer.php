3d69d6b4729c82b591b84a1e1b0061ece9692c12
ltm59 <ltm59@drexel.edu>
2022-01-31 18:07:03 -0500

    Header and footer updates

---
+++ wp-content/themes/idm250/footer.php
@@ -0,0 +1,13 @@
+<?php 
+
+wp_nav_menu(
+     [
+          'theme_location' => 'footer_menu'
+     ]
+);
+
+wp_footer(); ?>
+
+
+</body>
+</html>
---
+++ wp-content/themes/idm250/front-page.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1>This is the front-page.php file.</h1>
+
+<?php get_footer(); ?>
